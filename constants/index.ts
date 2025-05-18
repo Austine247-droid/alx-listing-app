10e280e772852ea207fd20681bf6857c33e34f29
Ambitiousdude <you@example.com>
2025-05-18 22:52:13 +0000

    updated files

---
+++ constants/index.ts
@@ -0,0 +1,72 @@
+// Importing icons
+import barn from "../public/assets/icons/barn.svg";
+import bed from "../public/assets/icons/bed.svg";
+import bathtub from "../public/assets/icons/bathtub.svg";
+import apartment from "../public/assets/icons/apartment.svg";
+import cabin from "../public/assets/icons/cabin.svg";
+import castle from "../public/assets/icons/castle.svg";
+import cottage from "../public/assets/icons/cottage.svg";
+import farm from "../public/assets/icons/farm.svg";
+
+// Importing images
+import image from "../public/assets/images/image.svg";
+import image1 from "../public/assets/images/image1.svg";
+import image2 from "../public/assets/images/image2.svg";
+import image3 from "../public/assets/images/image3.svg";
+import image4 from "../public/assets/images/image4.svg";
+import image6 from "../public/assets/images/image6.svg";
+import image7 from "../public/assets/images/image7.svg";
+import image8 from "../public/assets/images/image8.svg";
+import image9 from "../public/assets/images/image9.svg";
+import image12 from "../public/assets/images/image12.svg";
+import image17 from "../public/assets/images/image17.svg";
+import image18 from "../public/assets/images/image18.svg";
+import list1 from "../public/assets/images/list1.svg";
+import list2 from "../public/assets/images/list2.svg";
+import list3 from "../public/assets/images/list3.svg";
+import list4 from "../public/assets/images/list4.svg";
+import list5 from "../public/assets/images/list5.svg";
+import list6 from "../public/assets/images/list6.svg";
+import list7 from "../public/assets/images/list7.svg";
+import person1 from "../public/assets/images/person1.svg";
+import person2 from "../public/assets/images/person2.svg";
+import person3 from "../public/assets/images/person3.svg";
+
+// or, if you have an index.ts exporting icons in the icons folder:
+// import { barn } from "../public/assets/icons";
+
+export const Images = {
+  image,
+  image1,
+  image2,
+  image3,
+  image4,
+  image6,
+  image7,
+  image8,
+  image9,
+  image12,
+  image17,
+  image18,
+  list1,
+  list2,
+  list3,
+  list4,
+  list5,
+  list6,
+  list7,
+  person1,
+  person2,
+  person3,
+};
+
+export const Icons = {
+  barn,
+  bed,
+  bathtub,
+  apartment,
+  cabin,
+  castle,
+  cottage,
+  farm,
+};
